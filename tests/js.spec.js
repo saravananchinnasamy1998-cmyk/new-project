b71125dabc97c01a573c91a1657a319041c71206
Saravanan Chinnasamy <saravanan@Saravanans-MacBook-Air.local>
2026-08-21 16:21:38 +0530

    js.spec.js

---
+++ tests/js.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('verify Instagram URL', async ({ page }) => {
+  await page.goto('https://www.instagram.com/');
+
+  const title = await page.title();
+  console.log('Page title is: ' + title);
+
+  const url = page.url();
+  console.log('Page URL is: ' + url);
+
+  await expect(page).toHaveURL('https://www.instagram.com/');
+  await expect(page).toHaveTitle('Instagram');
+
+  console.log('Instagram URL and title verified');
+});
